0f3c3a257113c3db3cf6b676ebb3e277352e368d
Erdem Özgen <m.erdemozgen@gmail.com>
2018-07-15 13:34:04 +0300

    Add files via upload

---
+++ WebServiceCalculotor/CalculatorSOAPService/src/com/calculator/services/Calculator.java
@@ -0,0 +1,48 @@
+package com.calculator.services;
+
+public class Calculator 
+{
+	 private double result = 0;
+
+
+	    public double add(double addCandidateFirstParam , double addCandidateSecondParam)
+	    {
+	            result = addCandidateFirstParam + addCandidateSecondParam;
+	            return result;
+	    }
+
+
+	    public double divide(double divideCandidateFirstParam, double divideCandidateSecondParam)
+	    {
+	        if(areValidValuesForDivide(divideCandidateFirstParam, divideCandidateSecondParam)) {
+	            result = divideCandidateFirstParam / divideCandidateSecondParam;
+	        }
+
+	        return result;
+	    }
+
+
+	    public double multiply(double multiplyCandidateFirstParam, double multiplyCandidateSecondParam)
+	    {
+	            result = multiplyCandidateFirstParam * multiplyCandidateSecondParam;
+	            return result;
+	    }
+
+
+	    public double subtract(double subtractCandidateFirstParam, double subtractCandidateSecondParam)
+	    {
+	         result = subtractCandidateFirstParam - subtractCandidateSecondParam;
+	         return result;
+	    }
+
+
+	    private boolean areValidValuesForDivide(double validCandidateFirstParam, double validCandidateSecondParam)
+	    {
+	        if(validCandidateFirstParam==0.00 && validCandidateSecondParam==0.0) {
+	            return false;
+	        } else {
+	            return true;
+	        }
+	    }
+	}
+
