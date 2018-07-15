b79d5296697d0846f0380fb18d358b7046949588
Erdem Özgen <m.erdemozgen@gmail.com>
2018-07-15 13:45:00 +0300

    Add files via upload

---
+++ WebServiceCalculotor/CalculatorWithASPNET/CalculatorWebService/ClassLibrary/Calculator.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Calculator.aspx.cs" Inherits="CalculatorWebService.ClassLibrary.Calculator" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="margin-left: 40px">
+            <asp:Label ID="lbl_firstNumber_Input" runat="server" Text="1.Parameter:" Font-Size="Large"></asp:Label>
+            <asp:TextBox ID="txt_TextBox1" runat="server" Height="33px" Width="188px" ></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="lbl_secondNumber_Input" runat="server" Text="2.Parameter:" Font-Size="Large"></asp:Label>
+            <asp:TextBox ID="txt_TextBox2" runat="server" Height="33px" Width="188px" ></asp:TextBox>
+            <br />
+            <br />
+            <asp:DropDownList ID="ddl_CalculationTypes" runat="server" Height="66px" Width="323px">
+                <asp:ListItem>Please select</asp:ListItem>
+                <asp:ListItem>Add</asp:ListItem>
+                <asp:ListItem>Substract</asp:ListItem>
+                <asp:ListItem>Multiply</asp:ListItem>
+                <asp:ListItem>Divide</asp:ListItem>
+            </asp:DropDownList>
+            <br />
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btn_Calculate" runat="server" Height="83px" OnClick="btn_Calculate_Click" Text="Calculate" Width="142px" />
+            <br />
+            <br />
+            <asp:Label ID="lbl_result" runat="server" Font-Size="Large" Text="Result:"></asp:Label>
+            <asp:TextBox ID="txt_result" runat="server" Height="33px" Width="188px"></asp:TextBox>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebServiceCalculotor/CalculatorWithASPNET/CalculatorWebService/ClassLibrary/Calculator.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CalculatorWebService.ClassLibrary
+{
+    public partial class Calculator : System.Web.UI.Page
+    {
+        public static CalculatorServiceRefence.CalculatorClient client = new CalculatorServiceRefence.CalculatorClient();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void btn_Calculate_Click(object sender, EventArgs e)
+        {
+            double inputFromTextbox1 = Double.Parse(txt_TextBox1.Text);
+            double inputFromTextbox2 = Double.Parse(txt_TextBox2.Text);
+            double outputResult = 0.0;
+            try
+            {
+                switch (ddl_CalculationTypes.Text)
+                {
+                    case "Add":
+                        outputResult = client.add(inputFromTextbox1, inputFromTextbox2);
+                        break;
+                    case "Substract":
+                        outputResult = client.subtract(inputFromTextbox1, inputFromTextbox2);
+                        break;
+                    case "Multiply":
+                        outputResult = client.multiply(inputFromTextbox1, inputFromTextbox2);
+                        break;
+                    case "Divide":
+                        outputResult = client.divide(inputFromTextbox1, inputFromTextbox2);
+                        break;
+                    default:
+                        outputResult = 0.0;
+                        break;
+                }
+            }catch(Exception exception)
+            {
+
+                string directory = @"D:\Error.txt";  //default folder location
+                if (!Directory.Exists(directory))
+                {
+                    Directory.CreateDirectory(directory);
+                    File.AppendAllText(Server.MapPath("~/Error.txt"), "Message :" + exception.Message + "<br/>" + Environment.NewLine + "StackTrace :" + exception.StackTrace +
+               "" + Environment.NewLine + "Date :" + DateTime.Now.ToString());
+                    string New = Environment.NewLine + "-----------------------------------------------------------------------------" + Environment.NewLine;
+                    File.AppendAllText(Server.MapPath("~/Error.txt"), New);
+                }
+            }
+            txt_result.Text = outputResult.ToString();
+        }
+    }
+}
